4785e1771ecccd91895a8b4e84a2c901d7f47594
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2025-01-10 15:33:23 +0700

    view master data read only

--- resources/views/admin/bookmaster.blade.php
+++ resources/views/admin/bookmaster.blade.php
@@ -4,7 +4,6 @@
             {{ __('Book Master Data') }}
         </h2>
     </x-slot>
-    @dd($books)
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
@@ -14,22 +13,12 @@
                             <tr>
                                 <th scope="col" class="p-4">
                                     <div class="flex items-center">
-                                        <input
-                                            id="checkbox-all-search"
-                                            type="checkbox"
-                                            class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
-                                        />
-                                        <label for="checkbox-all-search" class="sr-only">checkbox</label>
+                                     id
                                     </div>
                                 </th>
-                                <th scope="col" class="px-6 py-3">Product name</th>
-                                <th scope="col" class="px-6 py-3">Color</th>
-                                <th scope="col" class="px-6 py-3">Category</th>
-                                <th scope="col" class="px-6 py-3">Accessories</th>
-                                <th scope="col" class="px-6 py-3">Available</th>
+                                <th scope="col" class="px-6 py-3">Book Name</th>
+                                <th scope="col" class="px-6 py-3">Author</th>
                                 <th scope="col" class="px-6 py-3">Price</th>
-                                <th scope="col" class="px-6 py-3">Weight</th>
-                                <th scope="col" class="px-6 py-3">Action</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -38,27 +27,16 @@
                                     class="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
                                 >
                                     <td class="w-4 p-4">
-                                        <div class="flex items-center">
-                                            <input
-                                                id="checkbox-table-search-1"
-                                                type="checkbox"
-                                                class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
-                                            />
-                                            <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                                        </div>
+                                        {{$book->id}}
                                     </td>
                                     <th
                                         scope="row"
                                         class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
                                     >
-                                        Apple MacBook Pro 17"
+                                        {{ $book->title }}
                                     </th>
-                                    <td class="px-6 py-4">Silver</td>
-                                    <td class="px-6 py-4">Laptop</td>
-                                    <td class="px-6 py-4">Yes</td>
-                                    <td class="px-6 py-4">Yes</td>
-                                    <td class="px-6 py-4">$2999</td>
-                                    <td class="px-6 py-4">3.0 lb.</td>
+                                    <td class="px-6 py-4">{{$book->author}}</td>
+                                    <td class="px-6 py-4">{{$book->price_per_day}}</td>
                                     <td class="flex items-center px-6 py-4">
                                         <a
                                             href="#"
