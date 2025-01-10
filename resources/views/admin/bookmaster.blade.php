975655e1effdae5a0e097082254d6bea040c0445
Muhammad Rasyad Caesarardhi <mrcrasyad26@gmail.com>
2025-01-10 15:25:48 +0700

    prettier result on blade view

--- resources/views/admin/bookmaster.blade.php
+++ resources/views/admin/bookmaster.blade.php
@@ -4,7 +4,7 @@
             {{ __('Book Master Data') }}
         </h2>
     </x-slot>
-@dd($books)
+    @dd($books)
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
@@ -34,43 +34,47 @@
                         </thead>
                         <tbody>
                             @foreach ($books as $book)
-                            <tr
-                            class="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
-                        >
-                            <td class="w-4 p-4">
-                                <div class="flex items-center">
-                                    <input
-                                        id="checkbox-table-search-1"
-                                        type="checkbox"
-                                        class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
-                                    />
-                                    <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                                </div>
-                            </td>
-                            <th
-                                scope="row"
-                                class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
-                            >
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td class="px-6 py-4">Silver</td>
-                            <td class="px-6 py-4">Laptop</td>
-                            <td class="px-6 py-4">Yes</td>
-                            <td class="px-6 py-4">Yes</td>
-                            <td class="px-6 py-4">$2999</td>
-                            <td class="px-6 py-4">3.0 lb.</td>
-                            <td class="flex items-center px-6 py-4">
-                                <a href="#" class="font-medium text-blue-600 hover:underline dark:text-blue-500">
-                                    Edit
-                                </a>
-                                <a href="#" class="ms-3 font-medium text-red-600 hover:underline dark:text-red-500">
-                                    Remove
-                                </a>
-                            </td>
-                        </tr>
+                                <tr
+                                    class="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
+                                >
+                                    <td class="w-4 p-4">
+                                        <div class="flex items-center">
+                                            <input
+                                                id="checkbox-table-search-1"
+                                                type="checkbox"
+                                                class="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600 dark:focus:ring-offset-gray-800"
+                                            />
+                                            <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                                        </div>
+                                    </td>
+                                    <th
+                                        scope="row"
+                                        class="whitespace-nowrap px-6 py-4 font-medium text-gray-900 dark:text-white"
+                                    >
+                                        Apple MacBook Pro 17"
+                                    </th>
+                                    <td class="px-6 py-4">Silver</td>
+                                    <td class="px-6 py-4">Laptop</td>
+                                    <td class="px-6 py-4">Yes</td>
+                                    <td class="px-6 py-4">Yes</td>
+                                    <td class="px-6 py-4">$2999</td>
+                                    <td class="px-6 py-4">3.0 lb.</td>
+                                    <td class="flex items-center px-6 py-4">
+                                        <a
+                                            href="#"
+                                            class="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                                        >
+                                            Edit
+                                        </a>
+                                        <a
+                                            href="#"
+                                            class="ms-3 font-medium text-red-600 hover:underline dark:text-red-500"
+                                        >
+                                            Remove
+                                        </a>
+                                    </td>
+                                </tr>
                             @endforeach
-
-
                         </tbody>
                     </table>
                 </div>
